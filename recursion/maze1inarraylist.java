009e970ef7d40c20f21bdaba9220c34294513f49
vedant kasar <vedantvvk5@gmail.com>
2024-01-07 19:32:49 +0530

    added new file

---
+++ recursion/maze1inarraylist.java
@@ -0,0 +1,36 @@
+package recursion;
+import java.util.*;
+
+
+public class maze1inarraylist {
+    static ArrayList<String> maze(String path , int row , int cols ){
+        // ArrayList<ArrayList<String>> list =new ArrayList<>();
+        if (row ==1 && cols ==1) {
+               ArrayList<String> list =new ArrayList<>();
+            // System.out.println(list);
+            list.add(path);
+            return list;
+
+        }
+        ArrayList<String> list =new ArrayList<>();
+        if (row>1) {
+            list.addall(maze(path+"D", row-1, cols));
+            
+        }
+        if (cols >1) {
+             list.addall(maze(path+"R", row, cols-1));
+            
+        }
+        return list;
+        
+
+
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> ans =maze("", 3, 3);
+        System.out.println(ans);
+        
+    }
+}
