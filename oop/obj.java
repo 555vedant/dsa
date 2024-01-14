6cf44c9b2fe814ac718d6790df02b2900c8ddabe
vedant kasar <vedantvvk5@gmail.com>
2024-01-14 23:14:46 +0530

    added new file

---
+++ oop/obj.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class obj {
+    public static void main(String[] args) {
+   final a vedant = new a(null);
+   vedant.name="other name";
+   System.out.println(vedant.name);
+
+
+    }
+}
