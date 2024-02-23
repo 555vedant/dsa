cb1094edc40ebd788c69dc18743e3b361cf3ef04
555vedant <vedantvvk5@gmail.com>
2024-02-24 00:10:12 +0530

    added new file in linkedlist

---
+++ hello.java
@@ -0,0 +1,7 @@
+//at 10 feb i am trying to dual boot at time whole data was deleted so from this i am fetching the work from git that you hava pushed the erlier
+public class hello{
+
+    public static void main(String[] args) {
+        System.out.println("hey new window");
+    }
+}
